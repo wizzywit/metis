dd2bcab02b7efba98113e8a36cc03802ee5fb3a0
Wizzywit <wisdompraise968@gmail.com>
2020-05-31 20:54:36 +0100

    updated the footer changes

--- resources/views/layouts/frontLayout/patientLayout/footer.blade.php
+++ resources/views/layouts/frontLayout/patientLayout/footer.blade.php
@@ -1,5 +1,5 @@
 <div class="footer">
     <div class="container">
-        <b class="copyright">&copy; 2019 Metis Technologies - </b>All rights reserved. Powered By <a href="https://wizzwit.github.io">Wizzywit</a>
+        <b class="copyright">&copy; 2019 Metis Technologies - </b>All rights reserved.
     </div>
 </div>
