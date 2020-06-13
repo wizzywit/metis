9c5f2b196057034bb4890910026d4d6a370580d4
Wizzywit <wisdompraise968@gmail.com>
2020-06-13 09:22:58 +0100

    coturn

--- public/js/app.js
+++ public/js/app.js
@@ -78915,24 +78915,7 @@ var Doctor = /*#__PURE__*/function (_Component) {
     _this.usersOnline;
     _this.caller;
     _this.localUserMedia = null;
-    _this.sessionDesc; // this.config = {
-    //     'iceServers': [
-    //         {
-    //             'url': 'stun:stun.l.google.com:19302'
-    //         },
-    //         {
-    //             'url': 'turn:numb.viagenie.ca',
-    //             'credential': 'Jesuschrist01',
-    //             'username': 'wisdompraise968@gmail.com'
-    //         },
-    //     ]
-    // };
-
-    _this.config = {
-      'iceServers': [{
-        'url': 'stun:127.0.0.1:4040'
-      }]
-    };
+    _this.sessionDesc;
     _this.mediaHandler; //To iron over browser implementation anomalies like prefixes
 
     _this.GetRTCPeerConnection();
@@ -79091,11 +79074,11 @@ var Doctor = /*#__PURE__*/function (_Component) {
       //Initializing a peer connection
       this.caller = new window.RTCPeerConnection({
         'iceServers': [{
-          'url': 'turn:numb.viagenie.ca',
-          'credential': 'Jesuschrist01',
-          'username': 'wisdompraise968@gmail.com'
+          'url': 'turn:turn.technigence.in',
+          'credential': 'metispass',
+          'username': 'metis'
         }, {
-          'url': 'stun:numb.viagenie.ca'
+          'url': 'stun:turn.technigence.in'
         }]
       });
       console.log(this.caller); //Listen for ICE Candidates and send them to remote peers
@@ -79395,24 +79378,7 @@ var Patient = /*#__PURE__*/function (_Component) {
     _this.usersOnline;
     _this.caller;
     _this.localUserMedia = null;
-    _this.sessionDesc; // this.config = {
-    //     'iceServers': [
-    //         {
-    //             'url': 'stun:stun.l.google.com:19302'
-    //         },
-    //         {
-    //             'url': 'turn:numb.viagenie.ca',
-    //             'credential': 'Jesuschrist01',
-    //             'username': 'wisdompraise968@gmail.com'
-    //         },
-    //     ]
-    // };
-
-    _this.config = {
-      'iceServers': [{
-        'url': 'stun:127.0.0.1:4040'
-      }]
-    };
+    _this.sessionDesc;
     _this.mediaHandler; //To iron over browser implementation anomalies like prefixes
 
     _this.GetRTCPeerConnection();
@@ -79631,11 +79597,11 @@ var Patient = /*#__PURE__*/function (_Component) {
       //Initializing a peer connection
       this.caller = new window.RTCPeerConnection({
         'iceServers': [{
-          'url': 'turn:numb.viagenie.ca',
-          'credential': 'Jesuschrist01',
-          'username': 'wisdompraise968@gmail.com'
+          'url': 'turn:turn.technigence.in',
+          'credential': 'metispass',
+          'username': 'metis'
         }, {
-          'url': 'stun:stun.l.google.com:19302'
+          'url': 'stun:turn.technigence.in'
         }]
       });
       console.log(this.caller); //Listen for ICE Candidates and send them to remote peers
