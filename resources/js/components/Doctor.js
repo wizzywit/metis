b559575b214bbfd71fbc73f229dd4c061a38dc11
Wizzywit <wisdompraise968@gmail.com>
2020-06-13 11:52:50 +0100

    coturn

--- resources/js/components/Doctor.js
+++ resources/js/components/Doctor.js
@@ -154,12 +154,12 @@ class Doctor extends Component {
       this.caller = new window.RTCPeerConnection({
             'iceServers': [
                 {
-                    'url': 'turn:turn.technigence.in:5349?transport=tcp',
+                    'url': 'turn:turn.technigence.in:5349',
                     'credential': 'metispass',
                     'username': 'metis'
                 },
                 {
-                    'url': 'stun:turn.technigence.in:5349?transport=tcp'
+                    'url': 'stun:turn.technigence.in:5349'
                 }
             ]
         });
--- resources/js/components/Patient.js
+++ resources/js/components/Patient.js
@@ -222,12 +222,12 @@ class Patient extends Component {
       this.caller = new window.RTCPeerConnection({
             'iceServers': [
                 {
-                    'url': 'turn:turn.technigence.in:5349?transport=tcp',
+                    'url': 'turn:turn.technigence.in:5349',
                     'credential': 'metispass',
                     'username': 'metis'
                 },
                 {
-                    'url': 'stun:turn.technigence.in:5349?transport=tcp'
+                    'url': 'stun:turn.technigence.in:5349'
                 }
             ]
         });
